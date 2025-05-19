c3777292d6e334273766d449e700d016691db5cd
ext_ugur.unlu@spark.net <ext_ugur.unlu@spark.net>
2025-05-19 21:53:52 +0200

    Remove dtos to map remote response

--- backend/src/main/java/com/f1/seasonchampions/service/SeasonChampionServiceImpl.java
+++ backend/src/main/java/com/f1/seasonchampions/service/SeasonChampionServiceImpl.java
@@ -2,6 +2,8 @@ package com.f1.seasonchampions.service;
 
 import com.f1.seasonchampions.dto.*;
 import com.f1.seasonchampions.model.*;
+import com.f1.seasonchampions.model.Constructor;
+import com.f1.seasonchampions.model.Driver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
