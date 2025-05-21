6ff1fe60c9f8fe65b8fabf1626541c6db703d7a4
uunlu <vally10@live.com>
2025-05-21 11:25:01 +0200

    Add 'flyway'

---
+++ backend/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE constructors
+(
+  id             BIGSERIAL PRIMARY KEY,
+  constructor_id VARCHAR(255) UNIQUE,
+  name           VARCHAR(255) NOT NULL,
+  nationality    VARCHAR(255)
+);
+
+CREATE TABLE drivers
+(
+  id               BIGSERIAL PRIMARY KEY,
+  driver_id        VARCHAR(255) UNIQUE,
+  permanent_number VARCHAR(10),
+  code             VARCHAR(5),
+  given_name       VARCHAR(255) NOT NULL,
+  family_name      VARCHAR(255) NOT NULL,
+  date_of_birth    VARCHAR(20),
+  nationality      VARCHAR(255)
+);
+
+CREATE TABLE season_champions
+(
+  id             BIGSERIAL PRIMARY KEY,
+  season         VARCHAR(255) NOT NULL UNIQUE,
+  driver_id      VARCHAR(255) REFERENCES drivers (driver_id),
+  constructor_id VARCHAR(255) REFERENCES constructors (constructor_id)
+);
+
+CREATE TABLE race_winners
+(
+  id             BIGSERIAL PRIMARY KEY,
+  season         VARCHAR(255) NOT NULL,
+  round          VARCHAR(255) NOT NULL,
+  driver_id      VARCHAR(255) REFERENCES drivers (driver_id),
+  constructor_id VARCHAR(255) REFERENCES constructors (constructor_id),
+  time           VARCHAR(255)
+);
