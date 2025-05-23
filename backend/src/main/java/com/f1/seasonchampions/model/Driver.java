abd7cad63f1201f262c00c89989d16c7a5f7958b
uunlu <vally10@live.com>
2025-05-24 00:24:00 +0200

    Add unit tests

--- backend/src/main/java/com/f1/seasonchampions/model/Driver.java
+++ backend/src/main/java/com/f1/seasonchampions/model/Driver.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "drivers")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Driver {
